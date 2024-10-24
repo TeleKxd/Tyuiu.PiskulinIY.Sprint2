cbb6bef3b1a7f289711f7c1efceeb047cb634e8f
TeleKxd <piskulin.i@mail.ru>
2024-10-24 20:35:55 +0300

    Task5

---
+++ Tyuiu.PiskulinIY.Sprint2.Task5.V11.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.PiskulinIY.Sprint2.Task5.V11.Lib;
+namespace Tyuiu.PiskulinIY.Sprint2.Task5.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfNextDay()
+        {
+            
+
+
+            DataService ds = new DataService();
+            Assert.AreEqual("1.1.2005", ds.FindDateOfNextDay(2004, 12, 31));
+
+
+        }
+    }
+}
